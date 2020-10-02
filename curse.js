130722cf844ff546ddb58026ade7dfa0d2e01abc
Kay Semple <rsemple12@gmail.com>
2020-10-02 08:17:52 -0400

    updated recursion2

--- curse.js
+++ curse.js
@@ -1,11 +1,13 @@
 /* Compute the sum of an array of integers. */
-
-let integers =[2, 5, 6, 9, 12]
-let sum = 0
-
-for(i = 0; i < integers.length; i++) {
-	sum = sum + integers[i];
+function sumOfIntegers(arr) {
+	let sum = 0 
+	for(i = 0; i < arr.length; i++) {
+	sum = sum + arr[i];
+}
+	console.log('The sum is: ' + sum)
 }
 
-console.log(sum);
-// Output is 34
+sumOfIntegers([2, 5, 6, 9, 13]);
+
+
+
